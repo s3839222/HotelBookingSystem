b12085dd4406ef857042a043bc550a41d17c9ad9
Abu Kuhafah s3839222 <s3839222@student.rmit.edu.au>
2021-05-08 12:12:26 +1000

    reset and register model

---
+++ src/main/model/ResetModel.java
@@ -0,0 +1,54 @@
+package main.model;
+
+import main.SQLConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class ResetModel {
+
+    Connection connection;
+
+    public ResetModel(){
+
+        connection = SQLConnection.connect();
+        if (connection == null)
+            System.exit(1);
+
+    }
+
+    public Boolean isDbConnected(){
+        try {
+            return !connection.isClosed();
+        }
+        catch(Exception e){
+            return false;
+        }
+    }
+
+    public boolean isReset(String newPassword, String username, String answer) throws SQLException {
+        PreparedStatement preparedStatement = null;
+        int resultSet= 0;
+        String query = "UPDATE employee SET password =? WHERE username = ? AND secret_answer =?";
+        try {
+//
+            preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, newPassword);
+            preparedStatement.setString(2, username);
+            preparedStatement.setString(3, answer);
+
+            preparedStatement.executeUpdate();
+            return true;
+        } catch (Exception e)
+        {
+            return false;
+        }finally {
+            preparedStatement.close();
+            //resultSet.close();
+        }
+
+    }
+
+
+}
